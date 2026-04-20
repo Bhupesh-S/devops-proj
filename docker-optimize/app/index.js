74b8dfd713d1b380cee48bcaf8c72a3664735170
Bhupesh-S <bhupeshsanthakumar.23cse@kongu.edu>
2026-04-20 05:56:27 +0000

    Checking jenkins13

--- docker-optimize/app/index.js
+++ docker-optimize/app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Docker'));
+app.get('/', (req, res) => res.send('Hello Docker11111'));
 app.listen(3000, () => console.log('Running on port 3000'));
